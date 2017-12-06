efc15d1e50063ae492bd9073bd254d41782280ec
JoaoCastro <up201404896@fe.up.pt>
2017-12-06 12:30:22 +0000

    start implementing trust on player

--- src/model/PlayerAgent.java
+++ src/model/PlayerAgent.java
@@ -1,4 +1,58 @@
 package model;
 
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.Ontology;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import model.onto.ServiceOntology;
+import sajas.core.AID;
+import sajas.core.Agent;
+import sajas.core.behaviours.SimpleBehaviour;
+import sajas.domain.DFService;
+import utils.StockPrice;
+
+import java.io.Serializable;
+import java.util.*;
+
 public class PlayerAgent {
+
+    private Codec codec;
+    private Ontology serviceOntology;
+    private String id;
+    private float capital;
+    private float portfolioValue;
+
+    public PlayerAgent(String id, float initialCapital)
+    {
+      this.id = id;
+      this.capital = initialCapital;
+      this.active = new ArrayList<>();
+      this.closed = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getCapital() {
+        return capital;
+    }
+
+    public float getPortfolioValue() {
+        return portfolioValue;
+    }
+
+    public float getTotalCapital() {
+        return capital + portfolioValue;
+    }
+
+    public int getProfile() {
+        return profile;
+    }
+
+
 }
